09955ff8a0b4043150800eb598fffad02815208f
alan rosales <alan.a.n.r.ar@gmail.com>
2023-10-07 13:14:49 -0300

    new project

---
+++ movies/movies.js
@@ -0,0 +1,30 @@
+const z = require('zod');
+
+const movieSchema = z.object({
+  title: z.string({
+    invalid_type_error: 'Mocie title must be a string'
+  }),
+  year: z.number().int().min(1900).max(2024),
+  director: z.string(),
+  duration: z.number().int().positive(),
+  rate: z.number().min(0).max(10),
+  poster: z.string().url(),
+  genre: z.array(
+    z.enum(
+      ['Action', 'Adventure', 'Crime', 'Comedy', 'Drama', 'Fantasy', 'Horror',
+        'Thriller', 'Sci-Fi']
+    ))
+})
+
+function validateMovie(object) {
+  return movieSchema.safeParse(object)
+}
+
+function validatePartialMovie(object) {
+  return movieSchema.partial().safeParse(object)
+}
+
+module.exports = {
+  validateMovie,
+  validatePartialMovie
+}
